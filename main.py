3dd35105149ab04084fb544cf11153089075c856
Salliii <99385051+Salliii@users.noreply.github.com>
2023-05-26 19:17:39 +0200

    added __clear_prompt__ function

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ class PromptLayout(object):
     def __reset_prompt__(self) -> int:
         print("\x1bc", end="")
         return 1
+    
+
+    def __clear_screen__(self) -> int:
+        print("\x1b[3J", end="")
+        return 1
