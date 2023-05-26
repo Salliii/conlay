a62de2a16dfc3f67a802f85fb60c42738764f348
Salliii <99385051+Salliii@users.noreply.github.com>
2023-05-26 19:19:46 +0200

    added __set_cursor_position__ function

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ class PromptLayout(object):
         return 1
     
 
-    def __clear_screen__(self) -> int:
+    def __clear_prompt__(self) -> int:
         print("\x1b[3J", end="")
+        return 1
+    
+    
+    def __set_cursor_position__(self, x, y) -> int:
+        print("\x1b[{y};{x}H".format(y=y+1, x=x+1), end="")
         return 1
