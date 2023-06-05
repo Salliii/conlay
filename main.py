73c4398b246806ad29237a7c274da9ab351f24f9
Salliii <silas.schulreich@gmail.com>
2023-06-06 00:21:43 +0200

    added comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 
 class Border:
+    """ clear border unicode character set """
+    
     vertical = u""
     horizontal = u""
     top_left = u""
@@ -10,6 +12,8 @@ class Border:
 
 
 class Bold(Border):
+    """ bold border unicode character set """
+
     def __init__(self) -> None:
         self.vertical = u"\u2503"
         self.horizontal = u"\u2501"
@@ -20,6 +24,8 @@ class Bold(Border):
 
 
 class Thin(Border):
+    """ thin border unicode character set """
+
     def __init__(self) -> None:
         self.vertical = u"\u2502"
         self.horizontal = u"\u2500"
@@ -32,16 +38,23 @@ class Thin(Border):
 
 
 class Color:
+    """ color set """
     
+    # clear color
     clear = "\x1b[0m"
 
-    class Bg:
 
+    class Bg:
+        """ background color set """
+
+        # clear background color
         clear = "\x1b[49m"
 
+        # function that returns a properly formatted ansi escape code for the background color
         def rgb(r:int, g:int, b:int) -> str:
             return "\x1b[48;2;{r};{g};{b}m".format(r=r, g=g, b=b)
         
+        # default color set
         black = rgb(0, 0, 0)
         white = rgb(255, 255, 255)
         red = rgb(205, 49, 49)
@@ -51,13 +64,18 @@ class Color:
         purple = rgb(188, 63, 188)
         cyan = rgb(17, 168, 205)
     
-    class Fg:
 
+    class Fg:
+        """ foreground color set """
+
+        # clear background color
         clear = "\x1b[49m"
 
+        # function that returns a properly formatted ansi escape code for the foreground color
         def rgb(r:int, g:int, b:int) -> str:
             return "\x1b[38;2;{r};{g};{b}m".format(r=r, g=g, b=b)
         
+        # default color set
         black = rgb(0, 0, 0)
         white = rgb(255, 255, 255)
         red = rgb(205, 49, 49)
@@ -71,40 +89,59 @@ class Color:
 
 
 class Console:
+    """ ansi escape sequences for console """
+
     def reset() -> int:
+        """ reset console and clear history """
+
         print("\x1bc", end="")
         return 1
 
 
     def clear() -> int:
+        """ clear console and keep history"""
+
         print("\x1b[3J", end="")
         return 1
     
 
     def eraseLineToEnd() -> int:
+        """ erase line from cursor to end """
+
         print("\x1b[0K", end="")
         return 1
     
 
     def eraseLinefromStart() -> int:
+        """ erase line from start to cursor """
+
         print("\x1b[1K", end="")
         return 1
     
 
     def eraseLine() -> int:
+        """ erase full line """
+
         print("\x1b[2K", end="")
         return 1
 
 
 
 
-class Cursor:   
+class Cursor:
+    """ ansi escape sequences for cursor """
+
     def setPosition(x:int, y:int) -> int:
+        """ set cursor to (x, y) coords """
         print("\x1b[{y};{x}H".format(y=y, x=x), end="")
         return 1
 
 
     def shiftHorizontal(sh: int) -> int:
+        """ shift cursor position on x axis
+            value > 0: shift to right
+            value < 0: shift to left """
+
         if sh < 0:
             print("\x1b[{sh}D".format(sh=sh*-1), end="")
             return 1
@@ -115,6 +152,10 @@ class Cursor:
 
 
     def shiftVertikal(sh:int) -> int:
+        """ shift cursor position on y axis
+            value > 0: shift down
+            value < 0: shift up """
+
         if sh < 0:
             print("\x1b[{sh}A".format(sh=sh*-1), end="")
             return 1
@@ -125,11 +166,15 @@ class Cursor:
     
 
     def hide() -> int:
+        """ hide cursor """
+
         print("\x1b[?25l", end="")
         return 1
     
     
     def show() -> int:
+        """ show cursor """
+
         print("\x1b[?25h", end="")
         return 1
 
@@ -137,13 +182,17 @@ class Cursor:
 
 
 class Conlay:
+    """ main layout """
 
+    # child list
     childs = []
 
+    # reset console and set cursor to (0, 0) after class call
     Console.reset()
     Cursor.setPosition(0, 0)
 
     def __init__(self) -> None:
+        # class attributes
         self.relative_x = int()
         self.relative_y = int()
         self.absolute_x = int()
@@ -159,81 +208,119 @@ class Conlay:
         self.padding_y = int()
         self.text = str()
         self.background = bool()
-        self.bg_color = Color.clear
-        self.fg_color = Color.clear
+        self.background_color = Color.clear
+        self.border_color = Color.clear
+        self.text_color = Color.clear
 
 
     def __sort_childs_by_zindex__(self, reverse=False) -> list:
+        """ sort child list by elements zindex attribute """
+        
         return list(sorted(self.childs, key=lambda child: child.zindex, reverse=reverse))
     
 
     def __sort_childs_by_height__(self, reverse=False) -> list:
+        """ sort child list by elements height attribute """
+
         return list(sorted(self.childs, key=lambda child: child.height, reverse=reverse))
     
 
     def __sort_childs_by_width__(self, reverse=False) -> list:
+        """ sort child list by elements width attribute """
+
         return list(sorted(self.childs, key=lambda child: child.width, reverse=reverse))
 
 
     def __get_final_cursor_position__(self) -> int:
+        """ calculate final cursor position """
+
         biggest_child = list(sorted(self.childs, key=lambda child: child.height + child.absolute_y, reverse=True))[0]
         return int(biggest_child.height + biggest_child.absolute_y) + 1
 
 
-    def add(self, element:None) -> int: #Element
+    def add(self, element:None) -> int: 
+        """ add element to layout element """
+
+        # set childs zindex to self zindex + 1
         element.zindex = self.zindex + 1
         
+        # calculate childs absolute position based on self position, padding and childs relative position
         element.absolute_x = self.absolute_x + self.padding_x + element.relative_x
         element.absolute_y = self.absolute_y + self.padding_y + element.relative_y
 
+        # calculate childs height, width based on min- and max- width/height
         if self.min_width != self.max_width and self.min_width < self.max_width and self.min_width >= 0:
             element.width = min(max(element.min_width, element.width), element.max_width)
             element.height = min(max(element.min_height, element.height), element.max_height)
 
+        # append child to layouts child list
         self.childs.append(element)
         
         return 1
     
 
     def print(self) -> int:
+        """ print layout """
 
         for element in self.__sort_childs_by_zindex__():
+            # iterate through elements in child list sorted by zindex
 
+            # try elements pre-printing function
             try:
                 element.__preprint__()
             except AttributeError:
                 pass
+            
 
+            # iterate through y axis
             for y in range(element.height):
+
+                # set cursors position
                 Cursor.setPosition(element.absolute_x, element.absolute_y + y + 1)
 
+
+                # iterate through x axis
                 for x in range(element.width):
+
+                    # print top left border
                     if x == 0 and y == 0:
-                        print(element.bg_color + element.fg_color + element.border.top_left, end=Color.clear)
+                        print(element.background_color + element.border_color + element.border.top_left, end=Color.clear)
+                    
+                    # print bottom left border
                     elif x == 0 and y == element.height - 1:
-                        print(element.bg_color + element.fg_color + element.border.bottom_left, end=Color.clear)
+                        print(element.background_color + element.border_color + element.border.bottom_left, end=Color.clear)
+                    
+                    # print top right border
                     elif x == element.width - 1 and y == 0:
-                        print(element.bg_color + element.fg_color + element.border.top_right, end=Color.clear)
+                        print(element.background_color + element.border_color + element.border.top_right, end=Color.clear)
+
+                    # print bottom right border
                     elif x == element.width - 1 and y == element.height - 1:
-                        print(element.bg_color + element.fg_color + element.border.bottom_right, end=Color.clear)
+                        print(element.background_color + element.border_color + element.border.bottom_right, end=Color.clear)
 
+                    # print vertical border
                     elif x == 0 or x == element.width - 1:
-                        print(element.bg_color + element.fg_color + element.border.vertical, end=Color.clear)
+                        print(element.background_color + element.border_color + element.border.vertical, end=Color.clear)
+                    
+                    # print horizontal border
                     elif y == 0 or y == element.height - 1:
-                        print(element.bg_color + element.fg_color + element.border.horizontal, end=Color.clear)
-
+                        print(element.background_color + element.border_color + element.border.horizontal, end=Color.clear)
+                    
+                    # background behavior
                     else:
                         if element.background:
-                            print(element.bg_color + element.fg_color + " ", end=Color.clear)
+                            print(element.background_color + element.border_color + " ", end=Color.clear)
                         else:
                             Cursor.shiftHorizontal(1)
 
-            
+
+            # try elements past-printing function
             try:
                 element.__pastprint__()
             except AttributeError:
                 pass
-
+        
+        # set cursor position
         Cursor.setPosition(0, self.__get_final_cursor_position__())
 
 
